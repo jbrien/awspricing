f3e65d341a39b24b92a34c6e1f9f64c07501e51a
Sean Kang <es.guybrush@gmail.com>
2014-03-07 14:47:49 +1300

    Updated URL of Amazon's pricing JSON. Added new product types such as I2, G2, M3, G3

--- awspricing/ec2.py
+++ awspricing/ec2.py
@@ -8,9 +8,15 @@ class EC2(Base):
     """ Class for EC2 pricing. """
     def __init__(self):
         Base.__init__(self)
-        self.json_data = json.loads(urllib.urlopen("http://aws.amazon.com/ec2/pricing/pricing-on-demand-instances.json").read())
-        self.currency = self.json_data['config']['currencies'][0]
-        self.rate = self.json_data['config']['rate']
+        pricing_list = { 'linux-od': "http://aws.amazon.com/ec2/pricing/json/linux-od.json",
+                         'rhel-od': "http://aws.amazon.com/ec2/pricing/json/rhel-od.json",
+                         'sles-od': "http://aws.amazon.com/ec2/pricing/json/sles-od.json",
+                         'mswin-od': "http://aws.amazon.com/ec2/pricing/json/mswin-od.json",
+                         'mswinSQL-od': "http://aws.amazon.com/ec2/pricing/json/mswinSQL-od.json",
+                         'mswinSQLWeb-od': "http://aws.amazon.com/ec2/pricing/json/mswinSQLWeb-od.json" }
+        self.json_data = dict()
+        for pricing_type in pricing_list:
+            self.json_data[pricing_type] = json.loads(urllib.urlopen(pricing_list[pricing_type]).read())
 
     def getSQL(self):
         """ Returns a list of SQL statements.
@@ -25,51 +31,53 @@ class EC2(Base):
         standard_pricing_prepaid  = 0
         software = ''
         queries = []
-        for region in self.json_data['config']['regions']:
-            region_id = awspricing.mapper.getRegionID(region['region'])
-            for instanceType in region['instanceTypes']:
-                prefix = instanceType['type']
-                for size in instanceType['sizes']:
-                    suffix = size['size']
-                    product_size = awspricing.mapper.getEC2ProductSize(prefix, suffix)
-                    if product_size == 'cc1.8xlarge':
-                        product_size = 'cc2.8xlarge'
-                    core_count = ec2desc.getVirtualCores(product_size)
-                    disk_in_gb = ec2desc.getStorageSize(product_size)
-                    memory_in_gb = ec2desc.getMemorySize(product_size)
-                    name = ec2desc.getName(product_size)
-                    description = ec2desc.getDescription(product_size)
-                    if product_size in ['t1.micro', 'c1.medium', 'm1.small', 'm1.medium']:
-                        architectures = ['I32', 'I64']
-                    else:
-                        architectures = ['I64']
-                    for value in size['valueColumns']:
-                        if value['name'] == "mswin":
-                            platform = "WINDOWS"
-                        elif value['name'] == "linux":
-                            platform = "UNIX"
+        for single_json in self.json_data.values():
+            currency = single_json['config']['currencies'][0]
+            for region in single_json['config']['regions']:
+                region_id = awspricing.mapper.getRegionID(region['region'])
+                for instanceType in region['instanceTypes']:
+                    for size in instanceType['sizes']:
+                        product_size = size['size']
+                        core_count = ec2desc.getVirtualCores(product_size)
+                        disk_in_gb = ec2desc.getStorageSize(product_size)
+                        memory_in_gb = ec2desc.getMemorySize(product_size)
+                        name = ec2desc.getName(product_size)
+                        description = ec2desc.getDescription(product_size)
+                        if product_size in ['t1.micro', 'c1.medium', 'm1.small', 'm1.medium']:
+                            architectures = ['I32', 'I64']
                         else:
-                            platform = "UNKNOWN"
-                        try:
-                            pricing_hourly = "%.3f" % float(value['prices'][self.currency])
-                        except ValueError:
-                            pricing_hourly = "0"
-                        for architecture in architectures:
-                            if platform == 'WINDOWS': 
+                            architectures = ['I64']
+                        for value in size['valueColumns']:
+                            if value['name'] == "mswin":
+                                platform = "WINDOWS"
+                            elif value['name'] == "linux":
+                                platform = "UNIX"
+                            elif value['name'] == "rhel":
+                                platform = "RHEL"
+                            elif value['name'] == "sles":
+                                platform = "SLES"
+                            elif value['name'] == "mswinSQL":
+                                platform = "MSWINSQL"
+                            elif value['name'] == "mswinSQLWeb":
+                                platform = "MSWINSQLWEB"
+                            try:
+                                pricing_hourly = "%.3f" % float(value['prices'][currency])
+                            except ValueError:
+                                pricing_hourly = "0"
+                            for architecture in architectures:
                                 query = "INSERT INTO server_product VALUES(%i, %s, '%s', '%s', '%s', %s, %2.1f, '%s', %s, %2.3f, '%s', '%s', %s, '%s', '%s', '%s', %s, %5.2f);" % (
-                                      server_product_id, self.cloud_id, product_size, active, architecture, core_count,
-                                      cpu_power, description, disk_in_gb, memory_in_gb, name, platform,
-                                      prepayment_term_in_months, region_id, software, self.currency,
-                                      pricing_hourly, standard_pricing_prepaid)
+                                    server_product_id, self.cloud_id, product_size, active, architecture, core_count,
+                                    cpu_power, description, disk_in_gb, memory_in_gb, name, platform,
+                                    prepayment_term_in_months, region_id, software, currency,
+                                    pricing_hourly, standard_pricing_prepaid)
                                 queries.append(query)
                                 server_product_id = server_product_id + 1
-                            else :
-                                for p in ['UNIX','UNKNOWN']:
+                                if platform == 'UNIX':
                                     query = "INSERT INTO server_product VALUES(%i, %s, '%s', '%s', '%s', %s, %2.1f, '%s', %s, %2.3f, '%s', '%s', %s, '%s', '%s', '%s', %s, %5.2f);" % (
-                                          server_product_id, self.cloud_id, product_size, active, architecture, core_count,
-                                          cpu_power, description, disk_in_gb, memory_in_gb, name, p,
-                                          prepayment_term_in_months, region_id, software, self.currency,
-                                          pricing_hourly, standard_pricing_prepaid)
+                                        server_product_id, self.cloud_id, product_size, active, architecture, core_count,
+                                        cpu_power, description, disk_in_gb, memory_in_gb, name, 'UNKNOWN',
+                                        prepayment_term_in_months, region_id, software, currency,
+                                        pricing_hourly, standard_pricing_prepaid)
                                     queries.append(query)
                                     server_product_id = server_product_id + 1
         return queries
@@ -83,22 +91,20 @@ class EC2(Base):
         csv = []
         csv.append("Product Size, Platform, Region ID, Currency, Pricing")
 
-        for region in self.json_data['config']['regions']:
-            region_id = awspricing.mapper.getRegionID(region['region'])
-            for instanceType in region['instanceTypes']:
-                prefix = instanceType['type']
-                for size in instanceType['sizes']:
-                    suffix = size['size']
-                    product_size = awspricing.mapper.getEC2ProductSize(prefix, suffix)
-                    if product_size == 'cc1.8xlarge':
-                        product_size = 'cc2.8xlarge'
-                    for value in size['valueColumns']:
-                        platform = value['name']
-                        try:
-                            pricing_hourly = "%.3f" % float(value['prices'][self.currency])
-                        except ValueError:
-                            pricing_hourly = "0"
-                        row = "%s, %s, %s, %s, %s" % (product_size, platform, region_id,
-                                                      self.currency, pricing_hourly )
-                        csv.append(row)
+        for single_json in self.json_data.values():
+            currency = single_json['config']['currencies'][0]
+            for region in single_json['config']['regions']:
+                region_id = awspricing.mapper.getRegionID(region['region'])
+                for instanceType in region['instanceTypes']:
+                    for size in instanceType['sizes']:
+                        product_size = size['size']
+                        for value in size['valueColumns']:
+                            platform = value['name']
+                            try:
+                                pricing_hourly = "%.3f" % float(value['prices'][currency])
+                            except ValueError:
+                                pricing_hourly = "0"
+                            row = "%s, %s, %s, %s, %s" % (product_size, platform, region_id,
+                                                          currency, pricing_hourly )
+                            csv.append(row)
         return csv
